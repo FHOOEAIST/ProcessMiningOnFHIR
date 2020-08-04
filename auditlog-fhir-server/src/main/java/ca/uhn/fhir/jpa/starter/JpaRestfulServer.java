d2e0d76aff116c6afde03a9d1320210c17b3ec32
a.lin <anna.lin@fh-hagenberg.at>
2020-08-04 14:55:16 -0400

    #9 fixed unspecific resource; device creation and search parameters

--- auditlog-fhir-server/src/main/java/ca/uhn/fhir/jpa/starter/JpaRestfulServer.java
+++ auditlog-fhir-server/src/main/java/ca/uhn/fhir/jpa/starter/JpaRestfulServer.java
@@ -1,5 +1,9 @@
 package ca.uhn.fhir.jpa.starter;
 
+import ca.uhn.fhir.jpa.api.dao.IFhirResourceDao;
+import org.hl7.fhir.r4.model.Device;
+import org.springframework.context.ApplicationContext;
+
 import javax.servlet.ServletException;
 
 public class JpaRestfulServer extends BaseJpaRestfulServer {
@@ -10,7 +14,24 @@ public class JpaRestfulServer extends BaseJpaRestfulServer {
   protected void initialize() throws ServletException {
     super.initialize();
 
+    org.springframework.context.ApplicationContext appCtx = (ApplicationContext) getServletContext()
+      .getAttribute("org.springframework.web.context.WebApplicationContext.ROOT");
+
+    IFhirResourceDao<Device> deviceDao = appCtx.getBean("myDeviceDaoR4", IFhirResourceDao.class);
+
+
     // Add your own customization here
+
+    //add device resource
+    Device thisSoftware = new Device();
+    Device.DeviceDeviceNameComponent deviceName = new Device.DeviceDeviceNameComponent();
+    deviceName.setName("Software: FH Hagenberg AIST: 'Process mining on FHIR' Server");
+    thisSoftware.addDeviceName(deviceName);
+
+    deviceDao.create(thisSoftware);
+
+
+    //TODO: add plandefinition resource
 
   }
 
