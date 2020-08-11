c8fbe1692cde20731ce1eea7a0a93b0322db1e68
Oliver <oliver.krauss@fh-hagenberg.at>
2020-08-11 16:24:15 +0200

    #6 added XES mapping (old definition)

--- auditlog-fhir-server/src/main/java/ca/uhn/fhir/jpa/starter/XESProvider.java
+++ auditlog-fhir-server/src/main/java/ca/uhn/fhir/jpa/starter/XESProvider.java
@@ -1,19 +1,23 @@
 package ca.uhn.fhir.jpa.starter;
 
-import ca.uhn.fhir.jpa.api.dao.DaoRegistry;
-import ca.uhn.fhir.jpa.rp.r4.AuditEventResourceProvider;
+import ca.uhn.fhir.jpa.api.dao.IFhirResourceDao;
+import ca.uhn.fhir.jpa.searchparam.SearchParameterMap;
 import ca.uhn.fhir.rest.annotation.Operation;
-import ca.uhn.fhir.rest.annotation.OperationParam;
+import ca.uhn.fhir.rest.api.server.IBundleProvider;
 import ca.uhn.fhir.rest.server.IResourceProvider;
 import com.github.dnault.xmlpatch.repackaged.org.apache.commons.io.IOUtils;
 import org.hl7.fhir.instance.model.api.IBaseResource;
-import org.hl7.fhir.r4.model.AuditEvent;
-import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Provider that adds operations for retrieving XES
@@ -22,15 +26,24 @@ import java.io.IOException;
  */
 public class XESProvider implements IResourceProvider {
 
-  /** A registry of data access objects used to talk to the back end data store */
+  /**
+   * A registry of data access objects used to talk to the back end data store
+   */
   @Autowired
-  private DaoRegistry dao;
+  IFhirResourceDao<AuditEvent> myAuditEventDao;
+
+  @Autowired
+  IFhirResourceDao<Device> myDeviceDao;
+
+  @Autowired
+  IFhirResourceDao<PlanDefinition> myPlanDefinitionDao;
 
   /**
    * Returns a bundle with the given request no clue yet how that works without a XES resource
+   *
    * @throws IOException
    */
-  @Operation(name="$bundleXes", idempotent=true)
+  @Operation(name = "$bundleXes", idempotent = true)
   public Bundle patientTypeOperation() {
 
     Bundle retVal = new Bundle();
@@ -40,18 +53,155 @@ public class XESProvider implements IResourceProvider {
 
   /**
    * Returns XES with the given request
-   * @param theServletRequest request from client
+   *
+   * @param theServletRequest  request from client
    * @param theServletResponse XES data as response
    * @throws IOException
    */
-  @Operation(name="$xes", manualResponse=true, manualRequest=true, idempotent = true)
+  @Operation(name = "$xes", manualResponse = true, manualRequest = true, idempotent = true)
   public void xesTypeOperation(HttpServletRequest theServletRequest, HttpServletResponse theServletResponse) throws IOException {
     String contentType = theServletRequest.getContentType();
     byte[] bytes = IOUtils.toByteArray(theServletRequest.getInputStream());
 
     theServletResponse.setContentType("text/plain");
-    theServletResponse.getWriter().write("It's a XES world");
+
+    theServletResponse.getWriter().write(generateXES());
     theServletResponse.getWriter().close();
+  }
+
+  /**
+   * Simple helper function that generates the XES. As the functionality is currently "in motion" with several open change requests in FHIR we are
+   * String concatenating instead of a template engine.
+   *
+   * @return XES XML file as plain text.
+   */
+  private String generateXES() {
+
+    Map<String, List<AuditEvent>> tracesPerPatient = new HashMap<>();
+
+    IBundleProvider search = myAuditEventDao.search(SearchParameterMap.newSynchronous());
+    List<IBaseResource> resources = search.getResources(0, search.size());
+    resources.forEach(x -> {
+      if (x != null && x instanceof AuditEvent) {
+        AuditEvent event = (AuditEvent) x;
+        // NOTE: Currently we only accept The Radiology Workflow
+        if (event.hasExtension("http://aist.fh-hagenberg.at/fhir/extensions/auditevent-basedon-extension") &&
+          ("PlanDefinition/2".equals(((Reference) event.getExtensionByUrl("http://aist.fh-hagenberg.at/fhir/extensions/auditevent-basedon-extension").getValue()).getReference()))) {
+          AuditEvent.AuditEventAgentComponent agent = event.getAgent() != null && !event.getAgent().isEmpty() ? event.getAgent().get(0) : null;
+          String caseId = agent != null && agent.getWho() != null ? agent.getWho().getReference() : null;
+          // TODO Most AuditEvents can't actually be related to a patient that way;
+          if (!tracesPerPatient.containsKey(caseId)) {
+            tracesPerPatient.put(caseId, new LinkedList<>());
+          }
+          tracesPerPatient.get(caseId).add(event);
+
+        }
+      }
+    });
+
+
+    return "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n" +
+      "\n" +
+      "<log xes.version=\"1.0\" xes.features=\"nested-attributes\" openxes.version=\"1.0RC7\" xmlns=\"http://www.xes-standard.org/\">\n" +
+      "\t<extension name=\"Organizational\" prefix=\"org\" uri=\"http://www.xes-standard.org/org.xesext\"/>\n" +
+      "\t<extension name=\"Time\" prefix=\"time\" uri=\"http://www.xes-standard.org/time.xesext\"/>\n" +
+      "\t<extension name=\"Lifecycle\" prefix=\"lifecycle\" uri=\"http://www.xes-standard.org/lifecycle.xesext\"/>\n" +
+      "\t<extension name=\"Concept\" prefix=\"concept\" uri=\"http://www.xes-standard.org/concept.xesext\"/>\n" +
+      "\n" +
+      "\t<string key=\"concept:name\" value=\"Radiology Workflow\"/>\n" +
+      "\n" +
+      tracesPerPatient.entrySet().stream().map(x -> trace(x.getKey(), x.getValue())).collect(Collectors.joining("\n")) +
+      "</log>\n";
+  }
+
+  /**
+   * Creates one trace per patient
+   *
+   * @param caseId patient case id
+   * @param events events for that patient
+   * @return trace string
+   */
+  private String trace(String caseId, List<AuditEvent> events) {
+    return "\t<trace>\n" +
+      "\t\t<string key=\"concept:name\" value=\"" + caseId + "\"/>\n" +
+      events.stream().map(x -> mapEvent(x)).collect(Collectors.joining("\n")) +
+      "\t</trace>\n\n";
+  }
+
+  /**
+   * Maps events to XES. This is completely un-reusable as we cheat by mapping KNOWN actions and Resources back to the correct task names
+   *
+   * @param x event to be mapped
+   * @return string of event
+   */
+  private String mapEvent(AuditEvent x) {
+    String event = "";
+
+    switch (x.getAction().name()) {
+      case "C":
+        switch (x.getEntityFirstRep().getWhat().getType()) {
+          case "Appointment":
+            event = "<event>\n" +
+              "\t\t\t<string key=\"concept:name\" value=\"Schedule Appointment\"/>\n" +
+              "\t\t\t<string key=\"lifecycle:transition\" value=\"complete\"/>\n" +
+              "\t\t\t<date key=\"time:timestamp\" value=\"" + x.getRecorded().toString() + "\"/>\n" +
+              "\t\t</event>";
+            break;
+          case "Media":
+            event = "<event>\n" +
+              "\t\t\t<string key=\"concept:name\" value=\"Diagnosis\"/>\n" +
+              "\t\t\t<string key=\"lifecycle:transition\" value=\"complete\"/>\n" +
+              "\t\t\t<date key=\"time:timestamp\" value=\"" + x.getRecorded().toString() + "\"/>\n" +
+              "\t\t</event>";
+            break;
+          case "DiagnosticReport":
+            event = "<event>\n" +
+              "\t\t\t<string key=\"concept:name\" value=\"Report Writing\"/>\n" +
+              "\t\t\t<string key=\"lifecycle:transition\" value=\"complete\"/>\n" +
+              "\t\t\t<date key=\"time:timestamp\" value=\"" + x.getRecorded().toString() + "\"/>\n" +
+              "\t\t</event>";
+            break;
+        }
+        break;
+      case "U":
+        switch (x.getEntityFirstRep().getWhat().getType()) {
+          case "Appointment":
+            event = "<event>\n" +
+              "\t\t\t<string key=\"concept:name\" value=\"Patient Admission\"/>\n" +
+              "\t\t\t<string key=\"lifecycle:transition\" value=\"complete\"/>\n" +
+              "\t\t\t<date key=\"time:timestamp\" value=\"" + x.getRecorded().toString() + "\"/>\n" +
+              "\t\t</event>";
+            break;
+          case "Procedure":
+            event = "<event>\n" +
+              "\t\t\t<string key=\"concept:name\" value=\"Radiological Examination\"/>\n" +
+              "\t\t\t<string key=\"lifecycle:transition\" value=\"complete\"/>\n" +
+              "\t\t\t<date key=\"time:timestamp\" value=\"" + x.getRecorded().toString() + "\"/>\n" +
+              "\t\t</event>";
+            break;
+          case "DiagnosticReport":
+            event = "<event>\n" +
+              "\t\t\t<string key=\"concept:name\" value=\"Report Attestation\"/>\n" +
+              "\t\t\t<string key=\"lifecycle:transition\" value=\"complete\"/>\n" +
+              "\t\t\t<date key=\"time:timestamp\" value=\"" + x.getRecorded().toString() + "\"/>\n" +
+              "\t\t</event>";
+            break;
+        }
+        break;
+      case "E":
+        switch (x.getEntityFirstRep().getDetailFirstRep().getValue().toString()) {
+          case "*$fhirToCDA":
+            event = "<event>\n" +
+              "\t\t\t<string key=\"concept:name\" value=\"Report Transmission\"/>\n" +
+              "\t\t\t<string key=\"lifecycle:transition\" value=\"complete\"/>\n" +
+              "\t\t\t<date key=\"time:timestamp\" value=\"" + x.getRecorded().toString() + "\"/>\n" +
+              "\t\t</event>";
+            break;
+        }
+        break;
+    }
+
+    return event;
   }
 
   @Override
